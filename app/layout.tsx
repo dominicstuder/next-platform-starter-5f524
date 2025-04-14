6c495b48d9318a6fd6ceddb915a3ae2b677e2fe7
dominicstuder <dominicstuder@users.noreply.github.com>
2025-04-14 15:00:48 +1000

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,64 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Navbar } from '../components/nav';
+import localFont from "next/font/local";
+import type { Metadata } from "next";
+import MobileDropdown from "../components/MobileDropdown";
+
+
+import clsx from 'clsx';
 import Head from 'next/head';
 
-export const metadata = {
-    title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
-    }
+
+
+const ABCMonumentGrotesk = localFont({
+  src: [
+    {
+      path: './fonts/ABCMonumentGrotesk-Bold-Trial.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/ABCMonumentGrotesk-Regular-Trial.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: "--font-monument",
+})
+
+
+
+
+export const metadata: Metadata = {
+  title: "Studio By and Large",
+  description: "Design, etc.",
 };
 
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                    <Head>
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-                        </Head>
-                        <Navbar />
-                        <main className="grow">{children}</main>
-                        <Footer />
-                    </div>
-                </div>
-            </body>
-        </html>
-    );
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+
+  return (
+    <html
+      lang="en"
+      className={
+        clsx(
+        'bg-white antialised subpixel-antialiased	dark:bg-stone-950'
+      )}
+  > 
+  <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </Head>
+      <body
+        className={`${ABCMonumentGrotesk.className} ${ABCMonumentGrotesk.className} antialiased`}
+      >
+       <Navbar />
+          <MobileDropdown />
+        {children}
+        <Footer />
+      </body>
+
+    </html>
+  );
 }
